2a74163511963d8e944720fcdf5ddcf45dd4e54c
CI Robot <robot@inceptionpad.com>
2019-11-25 09:37:45 +0000

    ci: auto deploy

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,28 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-git clone https://tanlukang:${GITHUB_TOKEN}@github.com/tanlukang/inception-wiki.git
-
+# init 
 npm install gitbook-cli -g
 gitbook install 
 gitbook build
+
+# remove cache
+if [ -f ./SUMMARY.md ]; then
+    rm ./SUMMARY.md
+fi
+
+# fetch repo
+git clone https://tanlukang:${GITHUB_TOKEN}@github.com/tanlukang/inception-wiki.git
 cp -R ./_book/* ./inception-wiki/
 
+# init repo info
 cd inception-wiki
 git remote rm origin
 git remote add origin https://tanlukang:${GITHUB_TOKEN}@github.com/tanlukang/inception-wiki.git
 git config --global user.email "robot@inceptionpad.com"
 git config --global user.name "CI Robot"
 
+# push
 git add .
 git commit -m "ci: auto deploy"
 git push --set-upstream origin master
